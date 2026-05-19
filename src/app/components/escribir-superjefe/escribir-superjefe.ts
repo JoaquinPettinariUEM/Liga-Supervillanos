b1eacfa3a8b46820adfbf8832a7e5b711016b5f0
joaquin.pettinari <joaquin.pettinari@hotmail.com>
2026-05-19 15:45:26 +0200

    Form is working but i cant show errors

--- src/app/components/escribir-superjefe/escribir-superjefe.ts
+++ src/app/components/escribir-superjefe/escribir-superjefe.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-escribir-superjefe',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './escribir-superjefe.html',
-  styleUrl: './escribir-superjefe.css',
 })
-export class EscribirSuperjefe {}
+export class EscribirSuperjefe {
+  mensajeGuardado: any = null;
+  formulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      remitente: ['', [Validators.required, Validators.maxLength(20)]],
+      edad: ['', [Validators.required, Validators.max(99)]],
+      mensaje: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(14)]],
+    });
+  }
+
+  guardar() {
+    if (this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+    this.mensajeGuardado = this.formulario.value;
+
+    this.formulario.reset();
+  }
+}
